1a8becd01c933fc7bc75cb4fe30f25b0929d7e47
Patrick Mackle <pimackle@ucdavis.edu>
2022-07-21 16:41:05 -0700

    more javascript exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  // Regular expression says "search whole string for any characters that are NOT a-z"
+  const filteredString = string.toLowerCase().replace(/[^a-z]/g, "");
+  return filteredString.split("").reverse().join("") === filteredString;
 };
 
+//palindromes("works with multiple words");
 // Do not edit below this line
 module.exports = palindromes;
